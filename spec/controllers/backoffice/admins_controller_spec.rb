c9cb21de9054c2a8b68e419dabc227f4c7d09b4d
shoday10 <imada@fiilse.com>
2018-02-13 00:55:57 +0900

    fix admins test

--- spec/controllers/backoffice/admins_controller_spec.rb
+++ spec/controllers/backoffice/admins_controller_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe Backoffice::AdminsController, type: :controller do
   describe 'GET show' do
     it 'assigns the requested admin to @admin' do
       admin = create(:admin)
-      # get :show, id: admin
-      # expect(assigns(:admin)).to eq admin
+      get :show, id: admin
+      expect(response.status).to eq(302)
     end
   end
 end
